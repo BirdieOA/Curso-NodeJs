fbda04fcffcc6af8bbe40f3acc146984cd92d644
BirdieOA <maitedeleonf@gmail.com>
2023-09-28 16:55:41 -0300

    agregandoImagen

--- clase-2/1.http.js
+++ clase-2/1.http.js
@@ -1,13 +1,42 @@
 const http = require('node:http')
+const fs = require('node:fs')
 
 const desiredPort = process.env.PORT ?? 1234
 
-const server = http.createServer((req, res) => {
-  console.log('request received: ', req.url)
-  res.end('Hola mundo')
-})
+const processRequest = (req, res) => {
+  res.setHeader('Content-type', 'text/html; charset=utf-8')
 
+  if (req.url === '/') {
+    res.statusCode = 200
+    res.end('<h1>Bienvenidos a mi página 🌼</h1>')
+
+  } else if (req.url === '/contacto') {
+    res.statusCode = 200 
+   res.end('<h1>CONTACTO</h1>')
+
+  } else if (req.url === '/protea.png') {
+    
+    fs.readFile('./protea.png', (err, data) => {
+      if (err) {
+        res.statusCode = 500
+        res.end('<h1>❌ 500 internal server error ❌</h1>')
+      } else {
+        res.setHeader('Content-Type', 'image/png')
+        res.end(data)
+        
+      }
+    })
+  } else {
+    res.statusCode = 404
+    res.end('<h1>404</h1>')
+
+  }
+  
+  }
+
+
+const server = http.createServer(processRequest)
 
   server.listen(desiredPort, () => {
-    console.log(`server listening on port http://localhost:${desiredPort}`)
+    console.log(`🌼 El puerto esta escuchando en ➡️ http://localhost:${desiredPort}`)
   })
