94736c5191f03679f3b8a70519bd23033cb303f7
BirdieOA <maitedeleonf@gmail.com>
2023-10-10 11:27:09 -0300

    actualizando

--- clase-3/app.js
+++ clase-3/app.js
@@ -1,14 +1,20 @@
-const express = require('express') // require --> commonsjs
+const express = require("express"); // require --> commonsjs
+const lovecraft = require('./biblioteca.json')
 
-const app = express()
-app.disable('x-powered-by') // Desabilitar el header X-Powered-by: Express
+const app = express();
+app.disable("x-powered-by"); // Desabilitar el header X-Powered-by: Express
 
-app.get('/', (req, res) => {
-  res.json({ message: 'escuchando 🌼' })
+app.get("/", (req, res) => {
+  res.json({ message: "escuchando 🌼" });
+});
+
+// Todos los recursos que sean Lovecraft se identificar con /lovecraft
+app.get('/lovecraft', (req, res) => {
+  res.json(lovecraft)
 })
 
-const PORT = process.env.PORT ?? 1234
+const PORT = process.env.PORT ?? 1234;
 
 app.listen(PORT, () => {
-  console.log(`Escuchando en el puerto http://localhost:${PORT}`)
-})
+  console.log(`Escuchando en el puerto http://localhost:${PORT}`);
+});
